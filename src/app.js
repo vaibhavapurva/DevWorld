a6fe3fbb31a4435025091f1f38f02f3359884acc
unknown <vaibhavapurvadewas@gmail.com>
2025-03-15 14:50:43 +0530

    sercer are created

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app  = express();
+
+
+
+app.use('/',(, res)=>{
+res.send("this is the dashboard page")    
+})
+
+app.use('/home',(req, res) =>{
+    res.send("app is called")
+})
+
+app.use('/text', (req, res)=>{
+    res.send("this is text path")
+})
+app.listen(3000,() =>{
+    console.log("Server is successfully listeing on port 3000")
+})
