a337a5d9a4d7ac41f67d286087c60fd4a6da969e
Vaibhav Apurva <vaibhavapurvadewas@gmail.com>
2025-03-16 11:38:08 +0530

    ceated get post and delete roults

--- src/app.js
+++ src/app.js
@@ -3,17 +3,25 @@ const app  = express();
 
 
 
-app.use('/',(req, res)=>{
-res.send("this is the dashboard page")    
+app.get('/user', (req, res) =>{
+    const data = {
+        name: "vaibhav apurva",
+        age: 24,
+        email: "vaibhavapurvadewas@gmail.com"
+    }
+    res.send(data)
 })
-
-app.use('/home',(req, res) =>{
-    res.send("app is called")
+app.delete('/user', (req, res) =>{
+    res.send("user was deleted")
+})
+app.post('/user', (req, res) =>{
+    res.send("data will succefull submited");
 })
 
 app.use('/text', (req, res)=>{
     res.send("this is text path")
 })
+
 app.listen(3000,() =>{
     console.log("Server is successfully listeing on port 3000")
 })
